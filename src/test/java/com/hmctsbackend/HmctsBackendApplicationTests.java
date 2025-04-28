ce1883cf87ed017ebcaa812ca6576ab1209daee4
Harsimran-dev <harsimran7394@gmail.com>
2025-04-28 13:52:59 +0100

    commit1

--- src/test/java/com/hmctsbackend/HmctsBackendApplicationTests.java
+++ src/test/java/com/hmctsbackend/HmctsBackendApplicationTests.java
@@ -1,13 +1,24 @@
 package com.hmctsbackend;
 
+import com.hmctsbackend.controllers.TaskController;
+import com.hmctsbackend.services.TaskService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HmctsBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TaskController taskController;
 
+    @Autowired
+    private TaskService taskService;
+
+    @Test
+    void contextLoads() {
+        // Verifies that the context loads and beans are injected correctly
+        assert taskController != null;
+        assert taskService != null;
+    }
 }
